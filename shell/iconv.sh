ecd3e720bb48b70f7f42b2dddfd5b5b0deb0a5d2
suntao <suntao904552435@outlook.com>
2018-07-09 14:58:27 +0800

    1. fix iconv.sh bug

--- shell/iconv.sh
+++ shell/iconv.sh
@@ -6,6 +6,10 @@ function usage() {
     echo $0 path/to/conv/file/or/dir
 }
 
+function getcharset(){
+    echo `file -i $1 | awk -F "charset=" '{print $2}'`
+}
+
 if test $# -ne 1 ; then
     usage
     exit 1
@@ -21,14 +25,15 @@ for file in ${filelist[*]} ; do
     if test -f $file ; then
         iconv -f gbk -t utf8 $file 2>/dev/null 1>/dev/null
         if test 0 -ne $? ; then
-            #echo "$file is not gbk"
+            echo "$file is not gbk"
             continue
         fi
         
-        iconv -f gbk -t utf8 $file -o $file
+        iconv -f gbk -t utf8 $file -o .temp_32urh23bj
         if test 0 -ne $? ; then
             echo "convert failed: $file"
         else
+            mv .temp_32urh23bj $file
             echo "convert ok: $file"
         fi
     fi
